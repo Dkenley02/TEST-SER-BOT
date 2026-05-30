70ad5a9800f7977984b97a2abf54a3c5fd004a32
Dkenley02 <kenleydaguerre@gmail.com>
2026-05-30 16:27:56 -0400

    Update verification.js

--- src/commands/Verification/verification.js
+++ src/commands/Verification/verification.js
@@ -194,6 +194,7 @@ async function handleSetup(interaction, guild, client) {
             .setLabel(buttonText)
             .setStyle(ButtonStyle.Success)
             .setEmoji("💠")
+            .setColor('#279CF5')
     );
 
     const verifyMessage = await verificationChannel.send({
