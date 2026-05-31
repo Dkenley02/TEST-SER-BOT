e3eb651d888ab9723b06108003169aeda0fe4ba7
Dkenley02 <kenleydaguerre@gmail.com>
2026-05-31 04:45:09 -0400

    Refactor dm command to improve channel handling

--- src/commands/Moderation/dm.js
+++ src/commands/Moderation/dm.js
@@ -1,83 +1,59 @@
-import { SlashCommandBuilder, PermissionFlagsBits, PermissionsBitField, ChannelType, MessageFlags } from 'discord.js';
-import { createEmbed, errorEmbed, successEmbed, infoEmbed, warningEmbed } from '../../utils/embeds.js';
-import { logEvent } from '../../utils/moderation.js';
-import { logger } from '../../utils/logger.js';
-import { sanitizeMarkdown } from '../../utils/sanitization.js';
+import {
+    SlashCommandBuilder,
+    PermissionFlagsBits,
+    MessageFlags
+} from 'discord.js';
 
+import { errorEmbed, successEmbed } from '../../utils/embeds.js';
+import { logger } from '../../utils/logger.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
+
 export default {
     data: new SlashCommandBuilder()
         .setName("say")
         .setDescription("Send a message to a channel")
-        .addUserOption(option =>
+        .addChannelOption(option =>
             option
                 .setName("channel")
-                .setDescription("Channel to send message to")
+                .setDescription("Channel to send the message to")
                 .setRequired(true)
         )
         .addStringOption(option =>
             option
                 .setName("message")
-                .setDescription("The message to send")
+                .setDescription("Message to send")
                 .setRequired(true)
         )
-        .addBooleanOption(option =>
-            option
-                .setName("anonymous")
-                .setDescription("Send the message anonymously (default: false)")
-                .setRequired(false)
-        )
-        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMassage)
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
         .setDMPermission(false),
+
     category: "Utility",
 
     async execute(interaction) {
         const deferSuccess = await InteractionHelper.safeDefer(interaction);
+
         if (!deferSuccess) {
-            logger.warn(`Say command defer failed`, {
-                userId: interaction.user.id,
-                guildId: interaction.guildId,
-                commandName: 'dm'
-            });
+            logger.warn("Say command defer failed");
             return;
         }
 
-    const targetUser = interaction.options.getUser("channel");
+        const channel = interaction.options.getChannel("channel");
         const message = interaction.options.getString("message");
-        const anonymous = interaction.options.getBoolean("anonymous") || false;
 
         try {
-            
             if (!channel.isTextBased()) {
                 return await InteractionHelper.safeEditReply(interaction, {
                     embeds: [
                         errorEmbed(
                             "Invalid Channel",
                             "Please select a text channel."
-                        ),
+                        )
                     ],
-                    flags: MessageFlags.Ephemeral,
+                    flags: MessageFlags.Ephemeral
                 });
             }
 
-            
             await channel.send({
-               content: message
-            });
-
-            return await InteractionHelper.safeEditReply(interaction, {
-                embeds: [
-                    successEmbed(
-                        "Message Sent",
-                        `Successfully sent message to ${channel}`
-                        ),
-                    ],
-                    flags: MessageFlags.Ephemeral,
-                });
-            }
-
-            
-                        await channel.send({
                 content: message
             });
 
